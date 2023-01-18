3cff36f7f643883479b30a48ce5295a791d8449b
Fabian <fabian.otto@de.bosch.com>
2023-01-18 16:00:41 +0100

    local changes

--- dmc.py
+++ dmc.py
@@ -6,10 +6,14 @@ from collections import deque
 from typing import Any, NamedTuple
 
 import dm_env
+import gym
+import mbrl_envs
 import numpy as np
 from dm_control import manipulation, suite
 from dm_control.suite.wrappers import action_scale, pixels
 from dm_env import StepType, specs
+from dm_env._environment import TimeStep
+from mbrl_envs import ObsTypes
 
 
 class ExtendedTimeStep(NamedTuple):
@@ -81,10 +85,10 @@ class FrameStackWrapper(dm_env.Environment):
             pixels_shape = pixels_shape[1:]
         self._obs_spec = specs.BoundedArray(shape=np.concatenate(
             [[pixels_shape[2] * num_frames], pixels_shape[:2]], axis=0),
-                                            dtype=np.uint8,
-                                            minimum=0,
-                                            maximum=255,
-                                            name='observation')
+            dtype=np.uint8,
+            minimum=0,
+            maximum=255,
+            name='observation')
 
     def _transform_observation(self, time_step):
         assert len(self._frames) == self._num_frames
@@ -180,25 +184,30 @@ class ExtendedTimeStepWrapper(dm_env.Environment):
         return getattr(self._env, name)
 
 
-def make(name, frame_stack, action_repeat, seed):
+def make(name, frame_stack, action_repeat, seed, obs_type: ObsTypes = ObsTypes.IMAGE):
     domain, task = name.split('_', 1)
     # overwrite cup to ball_in_cup
     domain = dict(cup='ball_in_cup').get(domain, domain)
     # make sure reward is not visualized
     if (domain, task) in suite.ALL_TASKS:
-        env = suite.load(domain,
-                         task,
-                         task_kwargs={'random': seed},
-                         visualize_reward=False)
+        # env = suite.load(domain,
+        #                  task,
+        #                  task_kwargs={'random': seed},
+        #                  visualize_reward=False)
         pixels_key = 'pixels'
     else:
-        name = f'{domain}_{task}_vision'
-        env = manipulation.load(name, seed=seed)
+        # name = f'{domain}_{task}_vision'
+        # env = manipulation.load(name, seed=seed)
         pixels_key = 'front_close'
+
+    env = mbrl_envs.make(domain, task, seed, obs_type, action_repeat=action_repeat, img_size=(84, 84))
+    gym.Ac
+    env = Gym2DM_Control(env)
     # add wrappers
     env = ActionDTypeWrapper(env, np.float32)
-    env = ActionRepeatWrapper(env, action_repeat)
-    env = action_scale.Wrapper(env, minimum=-1.0, maximum=+1.0)
+    # TODO Propreception dtype wrapper
+    # env = ActionRepeatWrapper(env, action_repeat)
+    # env = action_scale.Wrapper(env, minimum=-1.0, maximum=+1.0)
     # add renderings for clasical tasks
     if (domain, task) in suite.ALL_TASKS:
         # zoom in camera for quadruped
@@ -211,3 +220,53 @@ def make(name, frame_stack, action_repeat, seed):
     env = FrameStackWrapper(env, frame_stack, pixels_key)
     env = ExtendedTimeStepWrapper(env)
     return env
+
+
+class ObsDtypeWrapper(dm_env.Environment):
+    def __init__(self, env: dm_env.Environment, dtype):
+        self._env = env
+        wrapped_observation_spec = env.observation_spec()
+        self._observation_spec = specs.BoundedArray(wrapped_observation_spec.shape,
+                                                    dtype,
+                                                    wrapped_observation_spec.minimum,
+                                                    wrapped_observation_spec.maximum,
+                                                    'observation')
+
+    def step(self, action):
+        timestep = self._env.step(action)
+        # TODO convert dtype
+        return timestep
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def action_spec(self):
+        return self._env.action_spec()
+
+    def reset(self):
+        timestep = self._env.reset()
+        # TODO convert to dtype
+        return timestep
+
+    def __getattr__(self, name):
+        return getattr(self._env, name)
+
+
+class Gym2DM_Control(dm_env.Environment):
+
+    def __init__(self, env: gym.Env):
+        self._env = env
+
+    def reset(self) -> TimeStep:
+        obs, info = self._env.reset()
+
+        TimeStep(info['step_type'], )
+
+    def step(self, action) -> TimeStep:
+        pass
+
+    def observation_spec(self):
+        pass
+
+    def action_spec(self):
+        pass
